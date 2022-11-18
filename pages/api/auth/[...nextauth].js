58187e01fff6a3ac2a4e2af0c9ba11ac23bc598c
cwstechnology123 <monika.kumari@cwsinfotech.com>
2022-11-18 21:37:33 +0530

    init

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,10 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
+        // clientId: process.env.GOOGLE_ID,
+        // clientSecret: process.env.GOOGLE_SECRET,
+        clientId: '895180081492-3go2n3jup7uhq3efoo5f9iia1k8t08q7.apps.googleusercontent.com',
+        clientSecret: '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',
     }),
     // ...add more providers here
   ],
