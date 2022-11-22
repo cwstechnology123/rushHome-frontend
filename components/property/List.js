00063c1226c5296d09601a63d46202e9bd578424
cwstechnology123 <monika.kumari@cwsinfotech.com>
2022-11-22 19:25:07 +0530

    init

--- components/property/List.js
+++ components/property/List.js
@@ -9,7 +9,7 @@ export default function List() {
             <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabIndex={0}>
               <div className="row justify-content-center">
                 {[...Array(6)].map((e, i) => (
-                  <div className="col-xl-4 col-lg-6 col-md-6">
+                  <div key={"first"+i} className="col-xl-4 col-lg-6 col-md-6">
                       <PropertyCard index={(i+1)}/>
                   </div>
                 ))}
@@ -18,7 +18,7 @@ export default function List() {
             <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabIndex={0}>
               <div className="row justify-content-center">
                 {[...Array(6)].map((e, i) => (
-                  <div className="col-xl-4 col-lg-6 col-md-6">
+                  <div key={"second"+i} className="col-xl-4 col-lg-6 col-md-6">
                       <PropertyCard index={(i+1)}/>
                   </div>
                 ))}
@@ -27,7 +27,7 @@ export default function List() {
             <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabIndex={0}>
               <div className="row justify-content-center">
                 {[...Array(6)].map((e, i) => (
-                  <div className="col-xl-4 col-lg-6 col-md-6">
+                  <div key={"third"+i} className="col-xl-4 col-lg-6 col-md-6">
                       <PropertyCard index={(i+1)}/>
                   </div>
                 ))}
