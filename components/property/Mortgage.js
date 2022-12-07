a5fe04a8ea6a1dcd517239ec631ccd1b4305ead2
Jahir Tarafder <jahir@cwsinfotech.com>
2022-12-07 20:10:00 +0530

    module breakdown

--- components/property/Mortgage.js
+++ components/property/Mortgage.js
@@ -8,30 +8,32 @@ export default function Mortgage() {
                     <div className="calculation_wraper">
                     <form className="row g-3">
                         <div className="col-md-6">
-                        <label className="form-label">Total Amount</label>
-                        <input type="number" className="form-control" min={1} max={100} />
+                            <div className="form-group">
+                                <label className="form-label">Total Amount</label>
+                                <input type="number" className="form-control" />
+                            </div>
                         </div>
                         <div className="col-md-6">
-                        <label className="form-label">Down Payment</label>
-                        <input type="number" className="form-control"/>
+                            <label className="form-label">Down Payment</label>
+                            <input type="number" className="form-control"/>
                         </div>
                         <div className="col-md-6">
-                        <label className="form-label">Interest Rate</label>
-                        <input type="number" className="form-control"/>
+                            <label className="form-label">Interest Rate</label>
+                            <input type="number" className="form-control"/>
                         </div>
                         <div className="col-md-6">
-                        <label className="form-label">Number of Years</label>
-                        <input type="number" className="form-control"/>
+                            <label className="form-label">Number of Years</label>
+                            <input type="number" className="form-control"/>
                         </div>
                         <div className="col-md-12">
-                        <label htmlFor="inputState" className="form-label">Payment Period</label>
-                        <select id="inputState" className="form-select">
-                            <option>Monthly</option>
-                            <option>...</option>
-                        </select>
+                            <label htmlFor="inputState" className="form-label">Payment Period</label>
+                            <select id="inputState" className="form-select">
+                                <option>Monthly</option>
+                                <option>Yearly</option>
+                            </select>
                         </div>
                         <div className="col-12">
-                        <button type="submit" className="btn style1 button_custom">Calculate</button>
+                            <button type="submit" className="btn style1 button_custom">Calculate</button>
                         </div>
                     </form>
                     </div>
