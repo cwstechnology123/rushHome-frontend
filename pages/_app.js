c9fc5d9cece29ca5c9910c06285f249488bb24c3
cwstechnology123 <monika.kumari@cwsinfotech.com>
2022-12-08 20:46:12 +0530

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/layouts/Layout'
 import { SessionProvider } from "next-auth/react"
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({  Component,  pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <NextNProgress color="#29D" startPosition={0.3} stopDelayMs={100} height={3} showOnShallow={true} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
