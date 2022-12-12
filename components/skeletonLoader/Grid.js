4860d54fc11c05ff516394ac5557c3a9174030d7
cwstechnology123 <monika.kumari@cwsinfotech.com>
2022-12-12 20:46:31 +0530

    init

--- components/skeletonLoader/Grid.js
+++ components/skeletonLoader/Grid.js
@@ -6,7 +6,7 @@ export default function Grid({item}) {
         <div className="tab-content" id="pills-tabContent">
             <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabIndex={0}>
                 <div className="row justify-content-center">
-                {[...Array(item)].map(() => {
+                {[...new Array(item)].map(() => {
                     <div className="col-xl-4 col-lg-6 col-md-6">
                         <div className="property-card style3">
                             <Skeleton variant="rect" width={350} height={250} />
@@ -25,57 +25,6 @@ export default function Grid({item}) {
                         </div>
                     </div>
                 })}
-                <div className="col-xl-4 col-lg-6 col-md-6">
-                        <div className="property-card style3">
-                            <Skeleton variant="rect" width={350} height={250} />
-                            <div className="property-info">
-                                <div className="property-status-wrap">
-                                <p className="property-price"><Skeleton variant="text" width={350} /></p>
-                                </div>
-                                <h3><Skeleton variant="text" /></h3>
-                                <ul className="property-metainfo list-style">
-                                    <li key={`bed_x`}><i className="flaticon-double-bed" /><Skeleton variant="text"/> Br</li>
-                                    <li key={`bath_x`}><i className="flaticon-bath-tub" /><Skeleton variant="text"/> Ba</li>
-                                    <li key={`square_x`}><i className="flaticon-square" /><Skeleton variant="text"/> Sq.Ft</li>
-                                    <li key={`home_x`}><i className="flaticon-home" /><Skeleton variant="text"/> Gr</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-6 col-md-6">
-                        <div className="property-card style3">
-                            <Skeleton variant="rect" width={350} height={250} />
-                            <div className="property-info">
-                                <div className="property-status-wrap">
-                                <p className="property-price"><Skeleton variant="text" width={350} /></p>
-                                </div>
-                                <h3><Skeleton variant="text" /></h3>
-                                <ul className="property-metainfo list-style">
-                                    <li key={`bed_x`}><i className="flaticon-double-bed" /><Skeleton variant="text"/> Br</li>
-                                    <li key={`bath_x`}><i className="flaticon-bath-tub" /><Skeleton variant="text"/> Ba</li>
-                                    <li key={`square_x`}><i className="flaticon-square" /><Skeleton variant="text"/> Sq.Ft</li>
-                                    <li key={`home_x`}><i className="flaticon-home" /><Skeleton variant="text"/> Gr</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-6 col-md-6">
-                        <div className="property-card style3">
-                            <Skeleton variant="rect" width={350} height={250} />
-                            <div className="property-info">
-                                <div className="property-status-wrap">
-                                <p className="property-price"><Skeleton variant="text" width={350} /></p>
-                                </div>
-                                <h3><Skeleton variant="text" /></h3>
-                                <ul className="property-metainfo list-style">
-                                    <li key={`bed_x`}><i className="flaticon-double-bed" /><Skeleton variant="text"/> Br</li>
-                                    <li key={`bath_x`}><i className="flaticon-bath-tub" /><Skeleton variant="text"/> Ba</li>
-                                    <li key={`square_x`}><i className="flaticon-square" /><Skeleton variant="text"/> Sq.Ft</li>
-                                    <li key={`home_x`}><i className="flaticon-home" /><Skeleton variant="text"/> Gr</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
