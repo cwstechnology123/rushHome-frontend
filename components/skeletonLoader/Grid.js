e10de9a20e93b0a92ce7312efd39795622dc3063
cwstechnology123 <monika.kumari@cwsinfotech.com>
2022-12-12 20:56:22 +0530

    init

--- components/skeletonLoader/Grid.js
+++ components/skeletonLoader/Grid.js
@@ -25,6 +25,57 @@ export default function Grid({item}) {
                         </div>
                     </div>
                 })}
+                <div className="col-xl-4 col-lg-6 col-md-6">
+                        <div className="property-card style3">
+                            <Skeleton variant="rect" width={350} height={250} />
+                            <div className="property-info">
+                                <div className="property-status-wrap">
+                                <p className="property-price"><Skeleton variant="text" width={350} /></p>
+                                </div>
+                                <h3><Skeleton variant="text" /></h3>
+                                <ul className="property-metainfo list-style">
+                                    <li key={`bed_x`}><i className="flaticon-double-bed" /><Skeleton variant="text"/> Br</li>
+                                    <li key={`bath_x`}><i className="flaticon-bath-tub" /><Skeleton variant="text"/> Ba</li>
+                                    <li key={`square_x`}><i className="flaticon-square" /><Skeleton variant="text"/> Sq.Ft</li>
+                                    <li key={`home_x`}><i className="flaticon-home" /><Skeleton variant="text"/> Gr</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-6 col-md-6">
+                        <div className="property-card style3">
+                            <Skeleton variant="rect" width={350} height={250} />
+                            <div className="property-info">
+                                <div className="property-status-wrap">
+                                <p className="property-price"><Skeleton variant="text" width={350} /></p>
+                                </div>
+                                <h3><Skeleton variant="text" /></h3>
+                                <ul className="property-metainfo list-style">
+                                    <li key={`bed_x`}><i className="flaticon-double-bed" /><Skeleton variant="text"/> Br</li>
+                                    <li key={`bath_x`}><i className="flaticon-bath-tub" /><Skeleton variant="text"/> Ba</li>
+                                    <li key={`square_x`}><i className="flaticon-square" /><Skeleton variant="text"/> Sq.Ft</li>
+                                    <li key={`home_x`}><i className="flaticon-home" /><Skeleton variant="text"/> Gr</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-6 col-md-6">
+                        <div className="property-card style3">
+                            <Skeleton variant="rect" width={350} height={250} />
+                            <div className="property-info">
+                                <div className="property-status-wrap">
+                                <p className="property-price"><Skeleton variant="text" width={350} /></p>
+                                </div>
+                                <h3><Skeleton variant="text" /></h3>
+                                <ul className="property-metainfo list-style">
+                                    <li key={`bed_x`}><i className="flaticon-double-bed" /><Skeleton variant="text"/> Br</li>
+                                    <li key={`bath_x`}><i className="flaticon-bath-tub" /><Skeleton variant="text"/> Ba</li>
+                                    <li key={`square_x`}><i className="flaticon-square" /><Skeleton variant="text"/> Sq.Ft</li>
+                                    <li key={`home_x`}><i className="flaticon-home" /><Skeleton variant="text"/> Gr</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
