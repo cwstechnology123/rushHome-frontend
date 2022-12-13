ab873116e0eb51d98867b7ad368a6a7320772f78
cwstechnology123 <monika.kumari@cwsinfotech.com>
2022-12-13 20:03:21 +0530

    init

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialProvider from "next-auth/providers/credentials";
+import { apiBaseUrl } from '../../../utils/fetchApi'
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,11 +10,35 @@ export const authOptions = {
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
     }),
+    CredentialProvider({
+      async authorize(credentials) {
+        const response = await fetch(
+          `${apiBaseUrl}/login`,
+          {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        // Returning token to set in session
+        return {
+            token: data,
+          };
+      },
+    }),
     // ...add more providers here
   ],
   callbacks: {
-    async jwt({ token }) {
-      return token
+    jwt: async ({ token, user }) => {
+      user && (token.user = user);
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = token.user;  // Setting token in session
+      return session;
     },
   },
 }
--- pages/signup.js
+++ pages/signup.js
@@ -11,19 +11,23 @@ export default function SignUp() {
                 </div>
                 <form className="g-3">
                 <div className="col-md-12">
-                    <label htmlFor="inputEmail4" className="form-label">Name</label>
-                    <input type="text" className="form-control" placeholder="Full Name" />
+                    <label htmlFor="full_name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="full_name" placeholder="Full Name" />
                 </div>
                 <div className="col-md-12">
-                    <label htmlFor="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4" />
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" placeholder="Full Name"/>
                 </div>
                 <div className="col-md-12">
-                    <label htmlFor="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4" placeholder="Create Password" />
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Create Password" />
+                </div>
+                <div className="col-md-12">
+                    <label htmlFor="confirm_password" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" id="confirm_password" placeholder="Create Password" />
                 </div>
                 <div className="col-md-12 text-center">
-                    <button type="button" className="btn style1 button_agent">Sign Up</button>
+                    <button submit="button" className="btn style1 button_agent">Sign Up</button>
                 </div>
                 <div className="col-md-12 text-center">
                     <button type="button" className="btn style1 button_agent" onClick={() => signIn("google", { callbackUrl: '/client/dashboard' })}><span className="googleicon"><img src="assets/img/googleicon.png" /></span>Continue with Google</button>
