1e33bd6cda25ba161cebd13d1891463f8237f7fa
Jahir Tarafder <jahir@cwsinfotech.com>
2022-12-21 20:50:01 +0530

    property card info added

--- components/property/PropertyCard.js
+++ components/property/PropertyCard.js
@@ -14,13 +14,15 @@ const PropertyCard = ({
       areaTotal,
       slug,
       county,
+      postalCode,
       city,
       country,
       listPictureURL,
       listPicture2URL,
       listPicture3URL,
       garageSpaces,
-      fullStreetAddress
+      fullStreetAddress,
+      totalGarageAndParkingSpaces
     },
   }) => {
     // const [srcimg, setSrcimg] = useState(listPictureURL.replace(/^http:\/\//i, 'https://'));
@@ -36,12 +38,12 @@ const PropertyCard = ({
                     <div className="property-status-wrap">
                     <p className="property-price">{Number(listPrice).toLocaleString('en-US', { style: 'currency', currency: 'USD',minimumFractionDigits: 0 })}</p>
                     </div>
-                    <h3 className="text-dark" style={{whiteSpace: 'pre-wrap'}}>{fullStreetAddress ? `${fullStreetAddress}\n${county}, ${country}, ${city}`: ucfirst(county)}</h3>
+                    <h3 className="text-dark" style={{whiteSpace: 'pre-wrap'}}>{fullStreetAddress ? `${fullStreetAddress}\n${county}, ${country}, ${city} ${postalCode}`: ucfirst(county)}</h3>
                     <ul className="property-metainfo list-style">
                         <li key={`bed${id}`}><i className="flaticon-double-bed" />{bedroomsTotal? bedroomsTotal : '-'} Br</li>
                         <li key={`bath${id}`}><i className="flaticon-bath-tub" />{bathroomsTotal? bathroomsTotal : '-'} Ba</li>
                         <li key={`square${id}`}><i className="flaticon-square" />{areaTotal? Number(areaTotal).toLocaleString('en-US') : '-'} Sq.Ft</li>
-                        <li key={`home${id}`}><i className="flaticon-home" />{garageSpaces? garageSpaces : '-'} Gr</li>
+                        <li key={`home${id}`}><i className="flaticon-home" />{totalGarageAndParkingSpaces? totalGarageAndParkingSpaces : '-'} Gr</li>
                     </ul>
                 </div>
             </div>
