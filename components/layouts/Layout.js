400381520917a6c0292d9e3746319fe396dc7651
Jahir Tarafder <jahir@cwsinfotech.com>
2022-12-21 15:02:19 +0530

    revert changes

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,10 +1,8 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Head from 'next/head'
-import React from "react"
 
-export default function Layout({ getLayout, children }) {
-  console.log(getLayout, children)
+export default function Layout({ children }) {
   return (
     <>
       <Head>
@@ -14,16 +12,8 @@ export default function Layout({ getLayout, children }) {
       </Head>
       <div className="page-wrapper">
           <Navbar />
-          <main>
-            {/* {children} */}
-          {React.Children.map(children, (child) => {
-                if (React.isValidElement(child)) {
-                  //console.log("element set")
-                    return React.cloneElement(child);
-                }
-          })}
-          </main>
-          {!getLayout && <Footer />}
+          <main>{children}</main>
+          <Footer />
       </div>
     </>
   )
--- pages/homes-for-sale.js
+++ pages/homes-for-sale.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 import { apiBaseUrl, fetchApi } from "../utils/fetchApi";
 import SearchFilter from "../components/buy/SearchFilter";
 import BuyPropertyList from "../components/buy/BuyPropertyList";
+import BuyLayout from "../components/layouts/BuyLayout";
 import Footer from "../components/layouts/BuyFooter";
 import useWindowDimensions from "../components/buy/useWindowDimensions";
 import BuyMap from "../components/buy/BuyMap";
-import BuyLayout from "../components/layouts/BuyLayout";
 
 export default function HomesForSale({ properties }) { 
 
@@ -155,4 +155,6 @@ export async function getServerSideProps() {
     };
 }
 
-HomesForSale.getLayout = 1;
+HomesForSale.getLayout = function(page) {
+  return <BuyLayout>{page}</BuyLayout>;
+};
