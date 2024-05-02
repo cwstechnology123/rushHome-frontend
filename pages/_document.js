a0df335d0193b29cbe850628eb5ce9d18894864f
jahircws <jahir@cwsinfotech.com>
2024-05-02 21:12:09 +0530

    added matomo tracking tag

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,21 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="../assets/css/font-awesome.min.css" />
           <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
           <script defer src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}></script>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140459859-1"></script>
+          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140459859-1"></script> */}
+          <script dangerouslySetInnerHTML={{__html: `var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+            _paq.push(["setDomains", ["*.rushhome.vercel.app"]]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//pixeltracking.cwsbuild.com/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();`}}></script>
+          <noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="//pixeltracking.cwsbuild.com/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
           <script src="../assets/js/googleTagManager.js" async></script>
           <script src="../assets/js/fubWidgetTracker.js" async></script>
           <script src="../assets/js/jquery.min.js" async></script>
